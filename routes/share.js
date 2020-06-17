20deb6af9611852a58792ead5527461cdb0e534b
Sayan Mondal <sayanmondal342@gmail.com>
2020-06-17 23:14:29 +0530

    Routes Documented

--- routes/share.js
+++ routes/share.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+//@route    -   GET /share
+//desc      -   Route to display all social posts in a day
+//access    -   PUBLIC
 router.get("/", (req, res) => {
   res.send("Share METHOD");
 });
 
+//@route    -   GET /share/create
+//desc      -   Route to create a social post
+//access    -   PUBLIC
 router.get("/create", (req, res) => {
   res.send("Create Social Post");
 });
